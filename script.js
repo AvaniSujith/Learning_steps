3bb43536591ac6ac1db32c0d916356b2fe4ac427
AvaniSujith <chinchuavani123@gmail.com>
2025-04-25 15:13:39 +0530

    attempted creating the timeData object

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const startBtn = document.getElementById("start");
 const pauseBtn = document.getElementById("pause");
-const stopBtn = document.getElementById("stop");
 
 const hourDisplay = document.getElementById("hours");
 const minuteDisplay = document.getElementById("minutes");
 const secondDisplay = document.getElementById("seconds");
 
-
-let count = 0;
+let timeData = { fragments: [] };
+let timeCount = 0;
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
@@ -20,80 +19,72 @@ function updateTimerDisplay(){
 }
 
 function startTimer(){
+
     seconds++
     if(seconds === 60){
         seconds = 0;
         minutes++;
         if(minutes === 60){
             minutes = 0;
-            hours++
+            hours++;
         }
     }
-
-
-    // totalTime = setInterval(startTimer, 1000);
-
-    // // hourDisplay.innerText = `${hours}`
-    // hourDisplay.innerText = String(hours).padStart(2, "0");
-
-    // // minuteDisplay.innerText = `${minutes}`
-    // minuteDisplay.innerText = String(minutes).padStart(2, "0");
-
-    // // secondDisplay.innerText = `${seconds}`
-    // secondDisplay.innerText = String(seconds).padStart(2, "0");
 
     updateTimerDisplay();
 }
 
-
-// function stopTimer(){
-
-//     clearInterval(totalTime);
-//     totalTime = null;
-//     seconds = 0;
-//     minutes = 0;
-//     hours = 0;
-
-//     // hourDisplay.innerText = String(hours).padStart(2, "0");
-//     // minuteDisplay.innerText = String(minutes).padStart(2, "0");
-//     // secondDisplay.innerText = String(seconds).padStart(2, "0");
-
-//     updateTimerDisplay();
-
-// ;}
-
-// stopBtn.addEventListener("click", stopTimer);
-
-// startBtn.addEventListener("click", totalTime = setInterval(startTimer, 1000));
-
-
 startBtn.addEventListener("click", function(){
+
+    let timeStart;
+    let timeEnd;
     
-    // totalTime = setInterval(startTimer, 1000);
-
-    // startBtn.innerText = "stop";
-    // startBtn.classList.remove("start-btn");
-    // startBtn.classList.add("stop-btn");
-
-    // if(totalTime === null){
-    //     totalTime = setInterval(startTimer, 1000);
-    // }
 
     if(totalTime === null){
+
+        timeStart = new Date();
         totalTime = setInterval(startTimer, 1000);
         startBtn.innerText = "stop";
         startBtn.classList.remove("start-btn");
         startBtn.classList.add("stop-btn");
+
     }else{
+
+        timeEnd = new Date();
+        startBtn.innerText = "start";
+        startBtn.classList.remove("stop-btn");
+        startBtn.classList.add("start-btn");
+
         clearInterval(totalTime);
         hours = 0;
         minutes = 0;
         seconds = 0;
         totalTime = null;
         updateTimerDisplay()
-    }
-})
 
-// hours = Math.floor(totalTime/3600);
-// minutes = Math.floor((totalTime%3600)/60);
-// seconds = totalTime % 60;
+        let duration = timeStart - timeEnd;
+
+        timeData.label = `Timer ${count}`;
+        timeData.fragments.startTime = timeStart;
+        timeData.fragments.endTime = timeEnd;
+        timeData.fragments.duration = duration;
+
+    }
+
+    
+});
+
+function pauseTimer(){
+    clearInterval(totalTime);
+    totalTime=null;
+    pauseBtn.innerText = "Resume";
+}
+
+pauseBtn.addEventListener("click", function(){
+    if(totalTime !== null){
+        pauseTimer();
+    }else{
+        totalTime = setInterval(startTimer, 1000);
+        pauseBtn.innerText = "Pause";
+    }
+});
+
